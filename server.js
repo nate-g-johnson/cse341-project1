cf7997a763e104254c8318358d5f04ce5cbc6faf
nate-g-johnson <n_george_j@outlook.com>
2025-11-10 13:25:11 -0500

    installed swagger

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const mongodb = require('./data/database');
 const app = express();
 
 const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
+    next();
+});
 
 app.use('/', require('./routes'));
 
@@ -10,6 +22,8 @@ mongodb.initDb((err) => {
     if (err) {
         console.log(err);
     } else {
-        app.listen(port, () => {console.log(`Database is listening and node is running on port ${port}`)});
+        app.listen(port, () => {
+            console.log(`Database is listening and node is running on port ${port}`);
+        });
     }
 });
